8231dc82f204ecc8cb05f8a165c0a5464e42c62d
tamoorkayani <tamoorkayani@gmail.com>
2019-03-04 22:01:31 +0500

    Initial upload

---
+++ maincontents.js
@@ -0,0 +1,178 @@
+﻿var speech = new Array( );
+    
+    speech[1]="THANK YOU, GREG, AND WELCOME TO MANAGEMENT U! 2019. I’M JILL AND I WILL BE YOUR HOST THIS MORNING. I’M TOLD THAT LAST YEAR’S  TV HOSTS WERE UNFORTUNATELY CANCELLED DUE TO TOO MANY FAKE NEWS STORIES.  THAT’S TOO BAD, BUT LUCKY FOR ME!";
+    speech[2]="I’M COMING TO YOU LIVE FROM MINNEAPOLIS, MINNESOTA AT J L L -MANAGED ROSEDALE CENTER.  I’M DEFINITELY GOING TO NEED TO SPEAK TO MY AGENT TO EXPLAIN WHY YOU ARE IN SUNNY FLORIDA AND I’M IN A VIRTUAL SNOW GLOBE!  I’M SURE I HAVE SOMETHING IN MY CONTRACT THAT PROHIBITS ANY GIGS BELOW 75 DEGREES.  WELL, ENOUGH ABOUT ME.";
+    speech[3]="AS GREG STATED IN HIS INTRODUCTION, THIS YEAR’S CONFERENCE THEME IS AMBITION TO LEAD AND J L L RETAIL WILL CONTINUE TO BE THE LEADER IN RETAIL PROPERTY MANAGEMENT IF WE INVEST IN TECHNOLOGY TO HELP OUR PEOPLE OPERATE EFFICIENTLY, DRIVE MARGINS AND ENCHANCE CLIENT SERVICE.  AND I HAPPEN TO KNOW A THING OR TWO ABOUT TECHNOLOGY, SEEING THAT I’M MADE OF SENSORS, WIRES, COMPUTER PARTS, NUTS, BOLTS AND OTHER THINGS I CAN’T EVEN PRONOUNCE.  ALL OF THESE THINGS GIVE ME THIS GIRLISH FIGURE…..BUT ENOUGH ABOUT ME.";
+    speech[4]="BRICK AND MORTAR MUST EVOLVE FOR A NEW GENERATION OF SHOPPERS.  WE HAVE TO LOOK AT OLD PROCESSES AND REPLACE THEM WITH NEW, INNOVATIVE TOOLS AND AS PROPERTY MANAGERS, WE MUST LEARN WHAT CUSTOMERS WANT AND EXPECT WHEN THEY VISIT OUR CENTERS.  HERE IS A GREAT EXAMPLE OF UPDATING A CENTER AMENITY WITH NEW TECHNOLOGY. ";
+    speech[5]="RIGHT WHEN THE SHOPPER ENTERS THE CENTER, THEY ARE INVITED TO SEARCH FOR INFORMATION ON NEW TOUCH-SCREEN DIRECTORIES.  ACCORDING TO MARKET RESEARCH GROUP NIELSEN, AMERICAN ADULTS SPEND MORE THAN 11 HOURS PER DAY WATCHING, READING, LISTENING AND INTERACTING WITH MEDIA ON SCREENS, SO IT’S ONLY NATURAL THAT THEY WOULD EXPECT TO SEE DIGITAL SCREENS IN YOUR CENTER. EMBRACING TECHNOLOGY AND MAKING CHANGES IS NOT ALWAYS EASY.  IT CAN BE INTIMIDATING TO SEE MACHINES REPLACE TASKS PREVIOUSLY DONE BY HUMANS, BUT AS YOU WILL HEAR TODAY FROM J L L RETAIL LEADERS AND INDUSTRY EXPERTS, CHANGE IS COMING AND J L L RETAIL PROPERTY MANAGEMENT MUST BE READY.  ARE YOU?";
+    speech[6]="WOW!  HOW WILL J L L DISCOVER OPPORTUNITIES IN THE MOST TRANSFORMATIONAL TIME EVER?  LET’S HEAR FROM RETAIL CEO, GREG MALONEY TO LEARN MORE ABOUT THE DIRECTION RETAIL PROPERTY MANAGEMENT IS HEADING, IN THIS NEW ERA OF TECHNOLOGY.";
+    speech[7]="OH DEAR!  BLESS THEIR HEARTS!  WELL THANK GOODNESS GREG IS LOOKING TO ALL OF YOU TO LEAD THE CHANGE!  CAN YOU JUST IMAGE IF WE HAD GIVEN THEM ONE OF THESE:";
+    speech[8]="WELL, IT’S TIME TO SWITCH EARS…I MEAN GEARS…. I HEAR THAT RETAIL PROPERTY MANAGEMENT HAD A BANNER YEAR IN 2018.  21 NEW NATIONAL PROPERTIES AND 58 NEW OPEN-AIR CENTERS AS WELL AS 279 TOYS R US LOCATIONS. ";
+    speech[9]="I’M EXHAUSTED JUST READING THOSE NUMBERS.  CONGRATULATIONS TO ALL OF YOU WHO MADE THIS HAPPEN!  I’D LOVE TO HEAR MORE FROM OUR RETAIL PROPERTY MANAGEMENT LEADERS.  I HEAR IN THEIR FREE TIME, THEY HAVE ALSO BEEN BUSY FORMING A CLUB….THE RETAIL CLUB! ";
+    speech[10]="WELCOME BACK!  I HOPE YOU ALL HAD A CHANCE TO RECHARGE YOUR BATTERIES – I SURE DID!  MORE THAN LIKELY EVERYONE OF YOU SPENT SOME TIME ON YOUR MOBILE DEVICE DURING THE BREAK – CHECKING YOUR EMAIL, TEXTING A FRIEND, WATCHING YOUTUBE, ENSURING YOUR SNAP-STREAK DOESN’T DIE.  YOU KNOW, THE USUAL.  SOME OF YOU ARE ACTUALLY STILL ON YOUR MOBILE DEVICES NOW AND I’M ACTUALLY RECORDING YOU NOT PAYING ATTENTION…. ";
+    speech[11]="OH MY, SOME OF YOU LOOK SCARED!!  DON’T YOU JUST LOVE TECHNOLOGY.  AND ALEXA THINKS SHE’S GOT SKILLS….";
+    speech[12]="DID YOU KNOW THAT MOBILE PHONE USERS ARE EXPECTED TO REACH 4.68 BILLION PEOPLE IN 2019.  IN 2018, 52.2% OF ALL WORLDWIDE ONLINE TRAFFIC WAS GENERATED FROM A MOBILE PHONE, UP 50.3% FROM 2017.  ACCORDING TO THE BOSTON CONSULTING GROUP, CONSUMERS WERE ASKED WHAT THEY WOULD GIVE UP INSTEAD OF THEIR MOBILE PHONE:";
+    speech[13]="3 IN 10 WOULD STOP SEEING THEIR FRIENDS";
+    speech[14]="45% SAID THEY WOULD FORGO GOING ON VACATION";
+    speech[15]="46% SAID THEY WOULD GIVE UP A DAY OFF EACH WEEK";
+    speech[16]="55% WOULD RATHER FORGO EATING OUT FOR ONE YEAR";
+    speech[17]="CAN YOU IMAGE A WORLD TODAY WITHOUT A MOBILE DEVICE?";
+    speech[18]="NEEDLESS TO SAY, WE NEED AND DEPEND ON MOBILE TECHNOLOGY AND THAT IS ONLY GOING TO BECOME MORE IMPORTANT AS CHANGES ARE COMING TO HOW WE RECEIVE AND SEND INFORMATION FROM OUR MOBILE DEVICES.  WE ASKED TWO EXPERTS IN THE FIELD OF MOBILE AND WEB TECHNOLOGY TO JOIN US TODAY TO SHOW US THE FUTURE OF MOBILE AND WEB.  PLEASE JOIN ME IN WELCOMING: DANIEL WAGSTAFF – CEO OF POCKETSTOP And CORBETT GUEST – CEO OF IMAGINUITY INTERACTIVE";
+    speech[19]="THANK YOU, DANIEL & CORBETT.  LOOKS LIKE I MIGHT NEED A SYSTEM UPGRADE IF I’M GOING TO KEEP UP WITH THE RICH COMMUNICATION SYSTEM COMING OUR WAY SOON!";
+    speech[20]="WE’VE SPENT THE MORNING TALKING ABOUT CHANGES THAT WILL AFFECT RETAIL PROPERTY MANAGEMENT AS INNOVATION DRIVES EVERYTHING AT A RAPID PACE.  BUT WHAT ABOUT RETAILERS?  ARE THEY REACTING TO WHAT CUSTOMERS WANT AND IMPROVING THEIR RETAIL OPERATIONS TO ATTRACT AND WIN CUSTOMERS?  I HEAR THAT J L L HAS AN IN-HOUSE RETAIL EXPERT WHO STUDIES RETAILERS AROUND THE GLOBE TO FIND OUT EXACTLY HOW TECHNOLOGY IS CHANGING THEIR BUSINESS.  LET’S CHECK IN WITH J L L AMERICA’S DIRECTOR OF RESEARCH JAMES COOK:";
+    speech[21]="JAMES, IF YOU EVER NEED A COMPANION ON YOUR TRAVEL TO SEE RETAIL AROUND THE GLOBE, I’M AVAILABLE.  I DO REQUIRE A FIRST CLASS SEAT AND WILL NEED TO STOP FOR RECHARGING, BUT I’M READY WHEN YOU ARE.  THE JAMES & JILL PODCAST HAS A NICE RING TO IT, DON’T YOU THINK? WOW, WE’VE HAD A JAM-PACKED MORNING AT MANAGEMENT U!  LET’S RECAP WHAT WE’VE LEARNED TODAY AS WE ALL LOOK TO BECOME LEADERS THROUGH INNOVATION AND TECHNOLOGY:  J L L IS COMMITTED TO AND INVESTING IN TECHNOLOGY TO IMPROVE PROCESSES FOR EMPLOYEES, TO IMPROVE CLIENT SERVICE AND TO IMPROVE OUR MARGINS. WE WILL REMAIN THE LEADER IN RETAIL PROPERTY MANAGEMENT BY EMBRACING TECHNOLOGY AND INNOVATION.  TECHNOLOGY IS CHANGING QUICKLY – AND WE BETTER BE READY! BUT WE DON’T NEED TO BE SCARED! RETAILERS ARE CHANGING AND WE WILL TOO! AND LET’S NOT FORGET WE ARE ALREADY WINNING BECAUSE OF YOU AND EACH OF YOU ARE HONORARY MEMBERS OF THE RETAIL CLUB.   SINGS R-E-T-A-I-L……  WAIT, SINGING IS NOT IN MY CONTRACT";
+    speech[22]="WELL TEAM, IT’S TIME FOR ME TO SIGN OFF.  I’M MEETING UP WITH ROSIE";
+    speech[23]="R2-D2";
+    speech[24]="OPTIMUS PRIME";
+    speech[25]="AND WALL-E FOR LUNCH AND RUMOR HAS IT";
+    speech[26]="C3P-O IS BRINGING SIRI!  SHE USED TO DATE OPTIMUS PRIME, SO THIS IS GONNA BE GOOD.  ENJOY THE REST OF YOUR CONFERENCE AND REMEMBER:  YOU CAN SHINE NO MATTER WHAT YOU’RE MADE OF!";
+    
+    speech[27]="";
+
+
+    conf_count = 0;
+     
+       
+        var speak = new Array( );
+        speak[0] = "Novartis is seeing opportunity in ophthalmology ";
+        speak[1] = "Novartis is committed to finding the right solution for you.";
+        speak[2] = "Novartis has pioneered treatments for many retinal diseases.";
+        speak[3] = "Read a welcome message from our dean.";
+        speak[4] = "Find out what classes you need for your major.";
+        speak[5] = "";
+        speak[6] = "Say Cheese! ";
+        speak[7] = "Smile! You are about to become a robot.";
+        speak[8] = "Pick a song and let’s boogie!";
+
+//window.external.ChangeLanguage("en-us");
+function FC_ContentsCall(strContentsName, strLanguage)
+{
+   // alert(strContentsName);
+    
+    switch (strContentsName)
+    {
+        case "Welcome":
+            PlaySpeech("Hi, I’m Tracey, welcome to the Novartis booth. Please click around on my screen and then speak to my smart human friends in the booth.");
+            break;
+        case "Home":
+            writeCookie("NavigationState", true, 30);
+            location.href = "../../maincontents.htm";
+            break;
+        case "Menu":
+            location.href = "../../main.htm"; 
+            break;
+        case "MatchGame":
+            //PlaySpeech(speak[0]);
+            location.href = "Contents/MatchGame/index.html";
+            break;
+        case "Selfie":
+            //PlaySpeech(speak[6]);
+            location.href = "Contents/Selfie/index.html";
+            break;
+        case "Videos":
+            //PlaySpeech(speak[2]);
+            location.href = "Contents/Videos/index.html";
+            break;
+        case "Take-a-selfie":
+            //PlaySpeech(speak[1]);
+            location.href = "Contents/Take-a-selfie/index.html";
+            break;
+        case "Dancer":
+            //PlaySpeech(speak[1]);
+            location.href = "Contents/Dancer/index.html";
+            break;
+        
+        case "Avatar":
+            //ssPlaySpeech(speak[7]);
+            location.href = "Contents/RobotAvatar/index.htm";
+            break;
+        case "Dance":
+            //PlaySpeech(speak[8]);
+            location.href = "Contents/Dance/index.html";
+            break;
+        case "Config":
+            if(conf_count === 5)
+            {
+                conf_count = 0;
+                location.href = "Config/Config.htm";
+            }
+            else
+            {
+                conf_count++;
+            }
+
+            break;
+        case "go-mobile":
+            location.href = "Contents/go-moobile/index.html";
+            break;
+        default:
+            break;
+    } // end switch(strContentsName)
+} // end FC_ContentsCall
+
+
+
+function OnUserApproached()
+{
+    //PlaySpeech("Hello, welcome to the BWl Airport. Please press a button on my screen to begin.");
+}
+
+
+function ShowPopup(){
+
+// get the screen height and width
+    var maskHeight = $(document).height();
+    var maskWidth = $(window).width();
+    // calculate the values for center alignment
+    var dialogTop =  '30%';//(maskHeight/3) - ($('#dialog-box').height());
+    var dialogLeft = (maskWidth/2) - ($('#dialog-box').width()/2);
+    // assign values to the overlay and dialog box
+    $('#dialog-overlay').css({height:maskHeight, width:maskWidth}).show();
+    $('#dialog-box').css({top:dialogTop, left:dialogLeft}).show();
+    document.getElementById('dialog-box').innerHTML = '<a href="#" class="button">Close</a><div class="dialog-content"><div id="dialog-message"><img width="800" src="assets/contact.png"/></div></div>';
+}
+
+$(document).ready(function(){
+
+    //$('.navigation').hide();
+    $('.get-started').click(function(){
+        $('.welcome-text, .get-started').hide();
+        $('.slider-box').show();
+        PlaySpeech(speech[1]);
+
+    });
+})
+
+
+function OnJoystickControlled(strPara){
+    var btn_info = strPara.split(',')[4];
+
+
+    if(btn_info[0] == '1'){
+           // window.external.ChangeLanguage("en-us");
+            SetVolume(1);
+            window.external.PlaySpeech("");//I'm here to assist you, press any button on my screen to begin");
+          
+    }
+
+    if(btn_info[1] == '1'){
+        //window.external.ChangeLanguage("en-us");
+        SetVolume(1);
+        window.external.PlaySpeech("Come see me and I will take your picture");
+        
+    }
+    if(btn_info[2] == '1'){
+        //window.external.ChangeLanguage("en-us");
+            SetVolume(1);
+            window.external.PlaySpeech("Hi, Thanks for coming");
+    }
+    if(btn_info[3] == '1'){
+        //window.external.ChangeLanguage("en-us");
+        SetVolume(1);
+       window.external.PlaySpeech("");
+        
+    }
+    if(btn_info[4] == '1'){
+        FC_ContentsCall('Config');       
+    }
+}
